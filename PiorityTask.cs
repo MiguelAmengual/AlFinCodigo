fc10d6e58d49e39bbb17b6952eb7a5e7dc802df0
Miguel Angel Amengual <mamengual@hexacta.com>
2018-08-05 20:42:21 -0300

    The PiorityTask class was created and implemented

---
+++ PiorityTask.cs
@@ -0,0 +1,41 @@
+using System;
+namespace AlFinCodigo
+{
+    public class PiorityTask : Task
+    {
+        private int priority;
+        public PiorityTask(string title, int priority) : base(title)
+        {
+            this.Priority = priority;
+        }
+
+
+        public int Priority
+        {
+            get
+            {
+                return priority;
+            }
+
+            private set
+            {
+                EvaluatePriority(0, 999);
+                this.priority = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + ", Priority: " + Priority;
+        }
+
+        private void EvaluatePriority(int lowwerLimit, int supperLimit)
+        {
+            if (supperLimit > 999 || lowwerLimit < 0)
+            {
+                throw new ArgumentOutOfRangeException("The priority must be between 0 and 999");
+            }
+        }
+
+    }
+}
